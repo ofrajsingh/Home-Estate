2efcb356c9a0320ea6a4d00e3d11e8e8dea28e2b
ofrajsingh <rajsingh00seven@gmail.com>
2023-10-03 18:29:34 +0530

    update user completed

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,26 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema= new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     avatar: {
-        type: String,
-        default: "https://lh3.googleusercontent.com/a/ACg8ocLISgBwII2LFSt2acqkliQQpzQC76dh-LZbpRtCoIwu=s96-c"
-    }
-},{timestamps:true});
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+  },
+  { timestamps: true }
+);
 
-const User= mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
