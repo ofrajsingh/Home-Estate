762dfcdeeb3cc4c697e5bd5aa4692d8ccdf4d0c2
ofrajsingh <rajsingh00seven@gmail.com>
2023-10-03 19:18:54 +0530

    sign out added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,7 +27,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(401, "Wrong credentials!"));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const {password: pass, ...rest}= validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
@@ -37,26 +37,46 @@ export const signin = async (req, res, next) => {
   }
 };
 
-export const google= async (req,res,next)=>{
-  try{
-    const user= await User.findOne({email: req.body.email});
-    if(user){
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    const {password: pass, ...rest}= user._doc;
-    res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(rest);
-    }
-    else{
-      const generatedPassword= Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8);
-      const hashedPassword= bcryptjs.hashSync(generatedPassword,10);
-      const newUser= new User({ username:req.body.name.split(" ").join("").toLowerCase()+Math.random().toString(36).slice(-8), email:req.body.email, password: hashedPassword, avatar: req.body.photo });
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
       await newUser.save();
-      const token= jwt.sign({id: newUser._id},process.env.JWT_SECRET);
-      const {password: pass, ...rest}= newUser._doc;
-      res.cookie('access_token',token,{httpOnly: true}).status(200).json(rest);
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
     }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signOut= async (req,res,next)=>{
+  try{
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
   }
   catch(err){
     next(err);
